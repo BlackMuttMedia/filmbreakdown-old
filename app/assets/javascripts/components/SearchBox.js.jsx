51851313fa13fe5b3b0168288736e733716d295e
blkmutt <scott.sanzenbacher@blackmuttmedia.com>
2015-02-12 14:20:03 -0500

    Updated titleSearch and SearchBox code to use React.

--- app/assets/javascripts/components/SearchBox.js.jsx
+++ app/assets/javascripts/components/SearchBox.js.jsx
@@ -3,12 +3,109 @@ var SearchBox = React.createClass({
 		return (
 			  <div className="row collapse">
 	      		<div id="searchContainer" className="small-9 medium-9 large-10 columns">
-	      			<input type="text" className="titleSearch" placeholder="Search For Movies" />
+	      			<SearchBoxInput />
 	      		</div>
 	      		<div className="small-3 medium-3 large-2 end columns">
 	      			<a href="#" className="tiny button secondary">Go</a>
 	      		</div>
 	  		  </div>
 		)
+	}, 
+     isNullOrWhitespace: function(stringValue) {
+          return (typeof stringValue === 'undefined' || stringValue == null || stringValue.replace(/\s/g, '').length < 1);
+     },
+     getTmdbThumbnailUrl: function(poster_path)
+     {
+          return this.isNullOrWhitespace(poster_path) ? null : "http://image.tmdb.org/t/p/w45/" + poster_path;
+     },
+     getTmdbThumbnailTag: function(poster_path)
+     {
+     	var imageTag;
+
+         if(!this.isNullOrEmpty(poster_path))
+         {
+              imageTag = <img src={this.getTmdbThumbnailUrl(poster_path)} />;
+         }
+
+          return imageTag;
+     }
+});
+
+var SearchBoxInput = React.createClass({
+	render: function(){
+	    return(
+    		<input type="text" className="titleSearch" placeholder="Search For Movies" />
+    	);
+	},
+	componentDidMount: function() {
+
+	    $(this.getDOMNode()).autocomplete({
+	      source: function( request, response ) {
+	  		theMovieDb.search.getMovie({"query":escape(request.term), "search_type":"ngram", "include_adult":"false"}, 
+	  			function(data)
+	  			{ 
+	  				var results = _.first(JSON.parse(data).results, 8);
+	  				response(results); 
+	  			}, 
+	  			function(data) { response(null); });
+	      },
+	      minLength: 3,
+	      select: function( event, ui ) {
+	      	if(ui.item)
+	      	{
+	      		var newUrl = getFilmUrl(ui.item);
+	      		window.location.href = newUrl;
+	      	}
+	      },
+	      open: function() {
+	        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+	      },
+	      close: function() {
+	        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+	      },
+	      focus: function(event, ui) {
+	      	$(this).val(getFormattedTitle(ui.item));
+	      	event.preventDefault();
+	      },
+	      delay: 0
+	    })
+	    .autocomplete("instance")
+	    ._renderItem = function(ul, item)
+	    {
+	    	$(ul).addClass('search-dropdown small').data('dropdown-content');
+
+		  	var listItem = <SearchBoxItem baseImageUrl="http://image.tmdb.org/t/p/w45/" posterPath={item.poster_path} filmTitle={getFormattedTitle(item)} />;
+		  	var renderedListItem = React.renderToString(listItem);
+
+		  	$(ul).append(renderedListItem);
+		  	$(ul[0].lastChild).data("ui-autocomplete-item", item);
+		  	
+	    	return  ul;
+	    }
 	}
+})
+
+var SearchBoxItem = React.createClass({
+     render: function() {
+     	var searchBoxImage;
+
+     	if(!isNullOrWhitespace(this.props.posterPath))
+     	{
+     		searchBoxImage = <SearchBoxItemImage baseImageUrl={this.props.baseImageUrl} posterPath={this.props.posterPath} />;
+     	}
+	    return(
+          <li className="row">
+            <div className="small-3 medium-3 large-3 column">
+               {searchBoxImage}
+            </div>
+           <div className="small-9 medium-9 large-9 right">{this.props.filmTitle}</div>
+       	  </li>
+	    );
+     }
+});
+
+var SearchBoxItemImage = React.createClass({
+     render: function() {
+        return(<img src={this.props.baseImageUrl + this.props.posterPath} />);
+     }
 });
--- app/assets/javascripts/tmdb/titleSearch.js
+++ app/assets/javascripts/tmdb/titleSearch.js
@@ -45,66 +45,12 @@ getFilmUrl = function(item)
 	return url;
 };
 
-  $(document).ready(function() {
-    $('.titleSearch').autocomplete({
-      source: function( request, response ) {
-  		theMovieDb.search.getMovie({"query":escape(request.term), "search_type":"ngram", "include_adult":"false"}, 
-  			function(data)
-  			{ 
-  				var results = _.first(JSON.parse(data).results, 8);
-  				response(results); 
-  			}, 
-  			function(data) { response(null); });
-      },
-      minLength: 3,
-      select: function( event, ui ) {
-      	if(ui.item)
-      	{
-      		var newUrl = getFilmUrl(ui.item);
-      		window.location.href = newUrl;
-      	}
-        console.log( ui.item ?
-          ui.item :
-          "Nothing selected, input was " + this.value);
-      },
-      open: function() {
-        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
-      },
-      close: function() {
-        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
-      },
-      focus: function(event, ui) {
-      	$(this).val(getFormattedTitle(ui.item));
-      	event.preventDefault();
-      },
-      delay: 0
-    })
-    .autocomplete("instance")
-    ._renderItem = function(ul, item)
-    {
-    	$(ul).addClass('search-dropdown small').data('dropdown-content');
-    	var imageUrl = '';
 
-		var listItem = $("<li>").addClass('row');
-		listItem.data("ui-autocomplete-item", item);
-		$(ul).append(listItem);
+isNullOrWhitespace =  function(stringValue) {
+  return (typeof stringValue === 'undefined' || stringValue == null || stringValue.replace(/\s/g, '').length < 1);
+};
 
-		var anchor = $('<div>'); // { class: 'row'});
-		listItem.append(anchor);
-
-		var div =$('<div>').addClass('small-3').addClass('medium-3').addClass('large-3').addClass('column'); //'<div>', { class: 'small-3 medium-3 large-3 column'});
- 		listItem.append(div);
-
-    	if(!(typeof item.poster_path === 'undefined' || item.poster_path == null || item.poster_path.replace(/\s/g, '').length < 1))
-    	{
-    		imageUrl = "http://image.tmdb.org/t/p/w45/" + item.poster_path ;
-    		div.append($('<img></img>').attr('src', imageUrl)); // '<img>', { src: imageUrl });
-    	}
-    	var titleDiv = $('<div>').addClass('small-9').addClass('right'); //'<div>', { class: 'small-9 right' })
-    	titleDiv.append(getFormattedTitle(item));
-
-		listItem.append(titleDiv);
-
-    	return  ul;
-    }
-  });
+getTmdbThumbnailUrl = function(poster_path)
+{
+  return this.isNullOrWhitespace(poster_path) ? null : "http://image.tmdb.org/t/p/w45/aaaaa" + poster_path;
+};
